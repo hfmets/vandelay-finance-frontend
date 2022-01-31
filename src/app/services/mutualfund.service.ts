1f2c889fb6e38c166a6d2083eea7efb9ba642dcf
Hayden Farnsworth <hfmets@gmail.com>
2022-01-31 08:39:25 -0600

    fix url

--- src/app/services/mutualfund.service.ts
+++ src/app/services/mutualfund.service.ts
@@ -12,18 +12,11 @@ export class MutualfundService {
   readonly historicalData;
   readonly iraURL;
   constructor(private http: HttpClient) {
-<<<<<<< HEAD
     this.fundURL =
       'https://mysterious-dawn-78553.herokuapp.com/https://mutualfunds.herokuapp.com/currentMutualFunds';
-=======
     this.fundURL = 'https://financialmodelingprep.com/api/v3/quote';
->>>>>>> 5e4029d031a63a0c246333ffea3e0987e4b430a7
     this.URL =
-<<<<<<< HEAD
       'https://mysterious-dawn-78553.herokuapp.com/https://mutualfunds.herokuapp.com/currentMutualFunds/redis';
-=======
-      'https://cors-anywhere.herokuapp.com/https://mutual-funds-db.herokuapp.com/currentMutualFunds/redis';
->>>>>>> fce5f2b443d0d03060c07dee9c298b054b530e81
     // this.URL = `https://financialmodelingprep.com/api/v3/quotes/mutual_fund?apikey=${process.env.PARAMKEY}`;
 
     this.historicalData = `https://financialmodelingprep.com/api/v3/historical-chart/4hour`;
