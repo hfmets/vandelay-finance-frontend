dcda0c82552842f4234ffe4d72b40494de6c4526
kaseemstephenson <kaseem.stephenson001@albright.edu>
2022-02-01 14:45:52 -0500

    dash

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -28,7 +28,11 @@ export class AuthService {
     });
   }
   //these functions beloww should not be in the auth service, lazy coding on my part - Kaseem Stephenson
-  
-  
+  getNews(){
+    return this.http.get("https://mysterious-dawn-78553.herokuapp.com/https://stocknewsapi.com/api/v1/category?section=general&items=12&token=u5dsbsshi5vjxwphlv8bmtx7inniaunlkhb9fgmb")
+  }
+  getEZTrader(ticker:string){
+   return this.http.get("https://mysterious-dawn-78553.herokuapp.com/https://ez-trader-analysis.herokuapp.com/"+ticker)
+  }
 
 }
